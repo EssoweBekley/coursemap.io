a839622a2d9b9a32c6507f6b1e149d95ac2a746e
Essowe Bekley <essowebekley@gmail.com>
2025-02-01 21:06:17 -0500

    Fixed coursemaps not loading

--- components/MajorSelector.js
+++ components/MajorSelector.js
@@ -7,6 +7,7 @@ export default function MajorSelector() {
 
   const handleSelect = (event) => {
     const major = event.target.value;
+    console.log("Selected Major:", major); // Log the selected major
     if (major) router.push(`/coursemaps/${major}`);
   };
 
@@ -16,6 +17,7 @@ export default function MajorSelector() {
       <select onChange={handleSelect} defaultValue="">
         <option value="" disabled>Select a major</option>
         <option value="cs">Computer Science</option>
+        <option value="me">Mechanical Engineering</option>
       </select>
     </div>
   );
